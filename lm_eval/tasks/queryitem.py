174a23400fe8f925e5b031eea59e564f0e6e71e8
mayufei <adf11223344@sina.com>
2023-05-26 13:35:35 +0800

    Merge pull request #522 from EleutherAI/fix-mgpt-fewshot

--- lm_eval/tasks/queryitem.py
+++ lm_eval/tasks/queryitem.py
@@ -172,12 +172,11 @@ class QueryItem_Task(MCTask_Modified):
         # TODO: Format the query prompt portion of the document example.
         return doc["query"]
 
-    """def doc_to_target(self, doc: dict) -> str:
+    def doc_to_target(self, doc: dict) -> str:
         # TODO: Fill in the `target` ("gold answer") variable.
         # The prepended `" "` is required to space out the `doc_to_text` and
         # `doc_to_target` strings.
-        target = str(doc["gold"])
-        return " " + target"""
+        return " " + doc["choices"][doc["gold"] - 1]
 
     # def construct_requests(self, doc, ctx):
     """Uses RequestFactory to construct Requests and returns an iterable of
