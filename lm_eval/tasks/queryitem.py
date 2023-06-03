0e88b7d8132196ab326d736d70ef3ab0ac91a592
mayufei <adf11223344@sina.com>
2023-06-03 20:43:12 +0800

    Merge pull request #522 from EleutherAI/fix-mgpt-fewshot

--- lm_eval/tasks/queryitem.py
+++ lm_eval/tasks/queryitem.py
@@ -56,23 +56,6 @@ class MCTask_Modified(MultipleChoiceTask):
         
         self.dataset = data_lst
 
-    """def load_data(self):
-        assert os.path.exists(self.DATASET_PATH)
-        data_df = pd.read_csv(self.DATASET_PATH)
-        print(data_df.shape)
-
-        data_lst = data_df.values.tolist()
-        # assert isinstance(data, list)
-        data_map = map(self.__list2dict, data_lst)
-        assert isinstance(data_map, map)
-        self.dataset = data_map
-        self.data = data_lst
-
-    def __list2dict(self, list_) -> dict:
-        assert isinstance(list_, list) and len(list_) == 4
-        query, title, cate_desc, label = list_
-
-        return MCTask_Modified.InputTemplate(query, title, cate_desc, label).entity()"""
 
     class InputTemplate:
         def __init__(self, query: str, title: str, cate_desc: str, label: int):
@@ -204,8 +187,8 @@ class QueryItem_Task(MCTask_Modified):
         # and return them as an iterable.
         # return []
 
-    # def process_results(self, doc, results):
-    """Take a single document and the LM results and evaluates, returning a
+    def process_results(self, doc, results):
+        """Take a single document and the LM results and evaluates, returning a
         dict where keys are the names of submetrics and values are the values of
         the metric for that one document
 
@@ -213,11 +196,14 @@ class QueryItem_Task(MCTask_Modified):
             The document as returned from training_docs, validation_docs, or test_docs.
         :param results:
             The results of the requests created in construct_requests.
-    """
+        """
         # TODO: For each (sub)metric in the task evaluation, add a key-value pair
         # with the metric name as key and the corresponding metric result as value
         # for the current `doc`.
-        # return {}
+        print(f"results.__class__ == {type(results)}")
+        print(results)
+        assert False
+        return {}
 
     # def aggregation(self):
     """
