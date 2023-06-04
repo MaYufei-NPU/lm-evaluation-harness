f28a7c0862c3a7d4d75175802ff2d51c3d8eb430
A40 <martinichi@mail.nwwpu.edu.cn>
2023-06-04 14:52:07 +0800

    成功加入f1 metric

--- lm_eval/metrics.py
+++ lm_eval/metrics.py
@@ -262,10 +262,10 @@ def yesno(x):
 def queryitem_f1(arr):
     print(len(arr))
     print(arr.__class__)
-
-    numerator = sum(i for i in arr[0])
-    precision_denominator = sum(p for p in arr[1])
-    recall_denominator = sum(r for r in arr[2])
+    
+    numerator = sum(i[0] for i in arr)
+    precision_denominator = sum(p[1] for p in arr)
+    recall_denominator = sum(r[2] for r in arr)
 
     p = numerator / precision_denominator
     r = numerator / recall_denominator
